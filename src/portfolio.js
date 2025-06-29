b53b93f10e09334faf0ae88ff5456a3768fc57f7
ANANTHSARVESH <69849072+ANANTHSARVESH@users.noreply.github.com>
2025-06-30 00:27:04 +0530

    updated portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Java Backend Developer',
   description:
     'I’m a passionate Java Backend Developer with 3+ years of experience building scalable and secure REST APIs using Spring Boot, Microservices, and MySQL in the BFSI domain. I enjoy solving real-world problems with clean code, strong logic, and efficient architecture.\nCurrently, I’m enhancing my skills in System Design, Kafka, and DSA, aiming to join top-tier product companies. I believe in continuous learning, sharing knowledge, and building side projects that reflect my skills and growth.',
-  resume: 'https://drive.google.com/file/d/1BCmT62IXlbTFw9GhsPEoMURPLkF1VT_e/view?usp=drive_link',
+  resume: 'https://drive.google.com/file/d/1xtLWi_UlXR23jCV_bmuazPlaNcwWuloG/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/ananthsarvesh724',
     github: 'https://github.com/ANANTHSARVESH',
@@ -55,7 +55,7 @@ const skills = [
   'Hibernate',
   'RESTful APIs',
   'JWT',
-  'PL/SQL',
+  'SQL',
   'JUnit',
   'Git',
   'Postman API',
